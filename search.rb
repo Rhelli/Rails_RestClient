f3e7da8dd5c8cf80f1f8eaaf8d61efa8ed5025b9
Rory Hellier <rory.hellier@googlemail.com>
2020-02-17 23:26:23 +0000

    Updated search.rb to include some fun text typing effects on the terminal

---
+++ search.rb
@@ -0,0 +1,42 @@
+require 'rest-client'
+
+def get_keywords
+  string = "What would you like to search for? "
+  string.each_char do |i|
+    print i
+    sleep(0.03)
+  end
+  sleep(1)
+  @keywords = gets.chomp
+end
+
+def ellipsis
+  8.times do
+    sleep(0.5)
+    print "."
+  end
+end
+
+def search(get_keywords)
+  url = 'https://www.bing.com/search?q='
+  @response = RestClient.get(url + "#{get_keywords}")
+  ellipsis
+  puts
+  puts @response.code
+  sleep(2)
+  puts @response.cookies
+  sleep(2)
+  puts @response.headers
+  sleep(2)
+  puts @response.body
+  sleep(2)
+  string = 'Congratulations! Here are your web results!'
+  string.each_char do |i| 
+    print i
+    sleep(0.02)
+  end
+  sleep(1)
+  puts
+end
+
+search(get_keywords)
